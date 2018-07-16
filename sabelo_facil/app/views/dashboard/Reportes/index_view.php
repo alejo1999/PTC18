37eb9114fc83aeb028be4baca539f1898e029ea1
eduardozu666 <38825902+eduardozu666@users.noreply.github.com>
2018-07-16 13:29:13 -0600

    REPORTES SAD
    2 REPORTES FALTA AUN MUCHO :(

--- sabelo_facil/app/views/dashboard/Reportes/index_view.php
+++ sabelo_facil/app/views/dashboard/Reportes/index_view.php
@@ -93,6 +93,36 @@
                         </div>
                 </div>
         <!-- finalliza card de reporte -->
+
+         <!-- inicia div de card de reportes -->
+               <!-- aqui copian el div para hacer cada card -->
+               <div class="col s12 m3 l4">
+                        <div class="card blue darken-1">
+                            <div class="card-content white-text">
+                                <span class="card-title">EDUARDO Administradores</span>
+                                <p>Se imprimiran la lista de administradores registrados en el sistema.</p>
+                            </div>
+                            <div class="card-action">
+                                <a href="../../../../../../PTC18/sabelo_facil/dashboard/Reportes/administradores/Lista_de_trabajadores.php">Visualizar</a>
+                                
+                            </div>
+                        </div>
+                </div>
+        <!-- finalliza card de reporte -->
+        <!-- aqui copian el div para hacer cada card -->
+        <div class="col s12 m3 l4">
+                        <div class="card blue darken-1">
+                            <div class="card-content white-text">
+                                <span class="card-title">Impimir mi Información</span>
+                                <p>Se imprimira tu información del perfil personal.</p>
+                            </div>
+                            <div class="card-action">
+                                <a href="../../../../../../PTC18/sabelo_facil/dashboard/Reportes/administradores/mi_perfil.php">Visualizar</a>
+                                
+                            </div>
+                        </div>
+                </div>
+        <!-- finalliza card de reporte -->
         </div>
         </div>
 
