a23b10391de52b0f124331c3e3276ef5302fbd7a
alejo1999 <alenetoo1999@gmail.com>
2018-09-01 22:38:24 -0600

    cambios de categoria

--- sabelo_facil/app/models/categoria.class.php
+++ sabelo_facil/app/models/categoria.class.php
@@ -3,10 +3,9 @@ class Categoria extends Validator{
     //Declaracion de proipedades 
     private $id = null;
 	private $nombre = null;
-<<<<<<< HEAD
 	private $imagen_url = null;
     private $descripcion = null;
-    
+    private $estado = null;
     //mmetodos para poder llenar las propiedades
     public function setId($value){
         if($this->validateId($value)){
@@ -16,11 +15,6 @@ class Categoria extends Validator{
             return false;
         }
     }
-=======
-	private $imagen = null;
-	private $descripcion = null;
-	private $estado = null;
->>>>>>> 37eb9114fc83aeb028be4baca539f1898e029ea1
 
     public function getId(){
         return $this->id;
@@ -40,13 +34,8 @@ class Categoria extends Validator{
 	
 	
 	public function setImagen($file){
-<<<<<<< HEAD
 		if($this->validateImage($file, $this->imagen_url, "../../web/img/categoria/", 500, 500)){
 			$this->imagen_url = $this->getImageName();
-=======
-		if($this->validateImage($file, $this->imagen, "../../web/img/categorias/", 2000, 2000)){
-			$this->imagen = $this->getImageName();
->>>>>>> 37eb9114fc83aeb028be4baca539f1898e029ea1
 			return true;
 		}else{
 			return false;
@@ -63,10 +52,7 @@ class Categoria extends Validator{
 			return false;
 		}
 	}
-<<<<<<< HEAD
     
-=======
->>>>>>> 37eb9114fc83aeb028be4baca539f1898e029ea1
 	public function setDescripcion($value){
 		if($value){
 			if($this->validateAlphanumeric($value, 1, 200)){
@@ -83,26 +69,6 @@ class Categoria extends Validator{
 	public function getDescripcion(){
 		return $this->descripcion;
     }
-<<<<<<< HEAD
-    //metodos para el CRUD
-    public function getCategorias(){
-		$sql = "SELECT ID_categoria, nombre_categoria, descripcion_categoria ,imagen_url FROM categoria WHERE Estado = 1 ORDER BY nombre_categoria ";
-		$params = array(null);
-		return Database::getRows($sql, $params);
-    }
-    public function searchCategoria($value){
-		$sql = "SELECT * FROM categoria WHERE Estado = 1 AND nombre_categoria LIKE ? OR descripcion_categoria LIKE ? ORDER BY nombre_categoria";
-		$params = array("%$value%", "%$value%");
-		return Database::getRows($sql, $params);
-    }
-    public function createCategoria(){
-		$sql = "INSERT INTO categoria(nombre_categoria, descripcion_categoria,imagen_url , estado) VALUES(?, ?, ?,1)";
-		$params = array($this->nombre, $this->descripcion,$this->imagen_url);
-		return Database::executeRow($sql, $params);
-    }
-    public function readCategoria(){
-		$sql = "SELECT nombre_categoria, descripcion_categoria,imagen_url FROM categoria WHERE ID_categoria = ?";
-=======
 
 	public function setEstado($value){
 		if($value){
@@ -142,16 +108,12 @@ class Categoria extends Validator{
 	}
 	public function readCategoria(){
 		$sql = "SELECT nombre_categoria, imagen_url, descripcion_categoria ,Estado FROM categoria WHERE ID_categoria = ?";
->>>>>>> 37eb9114fc83aeb028be4baca539f1898e029ea1
 		$params = array($this->id);
 		$categoria = Database::getRow($sql, $params);
 		if($categoria){
 			$this->nombre = $categoria['nombre_categoria'];
-<<<<<<< HEAD
-=======
 			$this->imagen = $categoria['imagen_url'];
 			$this->estado = $categoria['Estado'];
->>>>>>> 37eb9114fc83aeb028be4baca539f1898e029ea1
 			$this->descripcion = $categoria['descripcion_categoria'];
 			$this->imagen_url = $categoria['imagen_url'];
 			
@@ -159,17 +121,10 @@ class Categoria extends Validator{
 		}else{
 			return null;
 		}
-<<<<<<< HEAD
-    }
-    public function updateCategoria(){
-		$sql = "UPDATE categoria SET nombre_categoria = ?, descripcion_categoria = ? , imagen_url = ? WHERE ID_categoria = ?";
-		$params = array($this->nombre, $this->descripcion, $this->imagen_url, $this->id );
-=======
 	}
 	public function updateCategoria(){
 		$sql = "UPDATE categoria SET nombre_categoria = ?, imagen_url = ?,descripcion_categoria = ?, Estado = ? WHERE ID_categoria = ?";
 		$params = array($this->nombre, $this->imagen,$this->descripcion, $this->estado, $this->id);
->>>>>>> 37eb9114fc83aeb028be4baca539f1898e029ea1
 		return Database::executeRow($sql, $params);
 	}
 	public function deleteCategoria(){
